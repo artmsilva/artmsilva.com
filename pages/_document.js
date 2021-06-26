ecd0f6e39c22d23022fafbdbf9dcaa8635b9c29d
Arturo Silva <artmsilva@gmail.com>
2021-06-25 23:07:43 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,12 @@ class MyDocument extends Document {
     return (
       <Html className="background document" lang="en">
         <Head>
-          <title>Arturo Silva — Senior Engineer at The Washington Post</title>
+          <title>Arturo Silva — Technical Architect at The Washington Post</title>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta
             name="description"
-            content="Front end engineering leader with 8+ years of experience in building front end solutions for e-commerce websites, web applications, marketing landing pages, and user engagement flows. Passionate about creating beautiful accessible user experiences."
+            content="Front end engineering leader with 10+ years of experience in building front end solutions for e-commerce websites, web applications, marketing landing pages, and user engagement flows. Passionate about creating beautiful accessible user experiences."
           />
           <meta name="robots" content="noodp" />
         </Head>
