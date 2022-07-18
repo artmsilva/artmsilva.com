9a3071c39f89dbe6b5ace5a4180a968dad73531a
Arturo Silva <arturo.silva@washpost.com>
2022-07-17 22:13:50 -0400

    feat: add new favicon from my selfie

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,24 @@ class MyDocument extends Document {
     return (
       <Html className="background document" lang="en">
         <Head>
-          <meta charset="utf-8" />
+          <meta charSet="utf-8" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
           <meta
             name="description"
             content="Front end engineering leader with 10+ years of experience in building front end solutions for e-commerce websites, web applications, marketing landing pages, and user engagement flows. Passionate about creating beautiful accessible user experiences."
